12746c932108cdb38d7516d1dddb43cdace233bf
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-05-25 11:10:31 -0300

    adicionado read_lock para acessar os dados

--- reddragons/estruturas/model_service.py
+++ reddragons/estruturas/model_service.py
@@ -8,24 +8,30 @@ class ModelService (object):
         self._dados = models.dados.Dados()
 
     @property
+    @read_lock
     def imagem (self):
         return self._imagem
+
     @imagem.setter
     @read_lock
     def imagem (self, data):
         self._imagem = data
 
     @property
+    @read_lock
     def controle (self):
         return self._controle
+
     @controle.setter
     @read_lock
     def controle (self, data):
         self._controle = data
 
     @property
+    @read_lock
     def dados (self):
         return self._dados
+
     @dados.setter
     @read_lock
     def dados (self, data):
