af00f2e7a211534080be26c34506004619e34ef9
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-06-18 18:15:39 -0300

    integração entre mainwindow.ui e python

--- bin/teste_escolha.py
+++ bin/teste_escolha.py
@@ -1,15 +1,15 @@
 from PyQt5.QtWidgets import QApplication
+import sys
+from reddragons.interface import GUI_video
+from reddragons.utils import Logger
 from reddragons.visao import Processamento
 from reddragons.estruturas import ModelService
-import sys
-from reddragons.interface import GUI_main
-from reddragons.utils import Logger
 
 if __name__ == '__main__':
     model = ModelService()
     visao = Processamento(model)
     app = QApplication(sys.argv)
-    window = GUI_main(visao, model)
+    window = GUI_video(visao)
     window.show()
     print (Logger()._fps_mean)
     sys.exit(app.exec_())
--- reddragons/interface/pages/mainwindow.py
+++ reddragons/interface/pages/mainwindow.py
@@ -1,19 +1,78 @@
-import os
-import pickle
+"""Modulo responsável por gerenciar as ações da pagina de seleção de entrada
 
+a funcao entrada_valida() foi colocada aqui pra facilitar nesse momento mas a ideia é ela ser movida
+para o modulo utils assim que estiver devidamente implementada
+
+fiquem a vontade para mexer nesse arquivo e testar o quanto quiserem já que ele não afeta em nada 
+o nosso programa nesse momento
+"""
+
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
-
+from typing import Union
 from ..utils import ui_files
 
+def entrada_valida (entrada: Union[int,str]) -> bool:
+    """verifica se a entrada escolhida é valida
+
+    Args:
+        entrada (Union[int,str]): entrada escolhida (int para camera, str para arquivo)
+
+    Returns:
+        bool: true para entrada valuda false caso contrario
+    """
+    pass
 
 class GUI_video(QMainWindow):
-    def __init__(self, visao, model):
+    def __init__(self, visao):
         super(GUI_video, self).__init__()
         loadUi(f"{ui_files}/mainwindow.ui", self)
-        self.visao = visao
-        self.model = model
         self.show()
+        self.visao = visao
+        self.btnChooseCamera.clicked.connect(self._camera_handler) #botao camera
+        self.btnChooseArquivo.clicked.connect(self._arquivo_handler) #botao video
+        
+    def _camera_handler(self) -> int:
+        """aqui a gente da um jeito de fazer o usuario escolher uma camera
 
+        Returns:
+            int: numero da camera escolhida (geralmente 0 ou 1)
+        """
+        escolha = input ("Escolha uma opção de camera (0/1): ")
+        escolha = int(escolha)
+        print (f"O usuário escolheu a entrada {escolha}")
+        self._config_visao(escolha)
+        return escolha
 
+    def _arquivo_handler(self) -> str:
+        """aqui a gente pega o caminho para algum arquivo de video
 
+        Returns:
+            str: caminho até o arquivo de video
+        """
+
+        pass
+
+    def _next_handler(self):
+        """abre a proxima janela (main) e fecha essa
+        """
+        pass
+
+    def _config_visao (self, entrada) -> bool:
+        """chama a funçao alterar_src ou cria um objeto de camera, 
+        enfim a ideia aqui é colocar a visao pra pegar o video de onde
+        a gente quer que pegue
+
+        Returns:
+            bool: sucesso na operação
+        """
+
+        if not entrada_valida: # se a entrada nao for valida ja para aqui
+            return False
+        self.visao.alterar_src(entrada)
+        # se for atribui a entrada para a visao aqui 
+        # ...
+        # ...
+        return True # e depois retorna True
+            
