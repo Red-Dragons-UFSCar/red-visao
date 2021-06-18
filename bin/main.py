ed815f8d82d5ba21bec994a806cec43c2564bb92
cadu1996 <carlos.eduardo.gba@gmail.com>
2021-06-18 17:08:50 -0300

    bin/main.py alterado para testar interface

--- bin/main.py
+++ bin/main.py
@@ -2,14 +2,16 @@ from PyQt5.QtWidgets import QApplication
 from reddragons.visao import Processamento
 from reddragons.estruturas import ModelService
 import sys
-from reddragons.interface import GUI_main
+#from reddragons.interface import GUI_main
+from reddragons.interface import GUI_video
 from reddragons.utils import Logger
 
 if __name__ == '__main__':
     model = ModelService()
     visao = Processamento(model)
     app = QApplication(sys.argv)
-    window = GUI_main(visao, model)
+#    window = GUI_main(visao, model)
+    window = GUI_video(visao, model)
     window.show()
     print (Logger()._fps_mean)
     sys.exit(app.exec_())
