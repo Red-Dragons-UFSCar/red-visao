1e113ceb11ee75d969dc0660b32b0d7a035ef27a
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-07-04 15:13:44 -0300

    melhoria no IF/ELSE dos centroides

--- reddragons/visao/services/centroides.py
+++ reddragons/visao/services/centroides.py
@@ -4,7 +4,8 @@ from reddragons.estruturas import Dados, Imagem
 import cv2
 
 class Centroides:
-
+    """Classe resposável pro identificar os centroides dos robôs
+    """
     def __init__(self, dados: Dados):
         self._dados = dados
     
@@ -12,15 +13,13 @@ class Centroides:
     def run(self, imagem, dest: Imagem = None):
         centr_final = []
         for cor, filtro in zip(self._dados.cores, self._dados.filtros):
-            cortornos, _ = utils.get_contorno_cor(
+            contornos, _ = utils.get_contorno_cor(
                 imagem, cor, filtro
             )
             centroids = np.empty((0, 3))
-            for c in cortornos:
+            for c in contornos:
                 moments = cv2.moments(c)
-                if (moments["m00"] >= self._dados.area_minima) and (
-                    moments["m00"] <= self._dados.area_maxima
-                ):
+                if self._dados.area_minima <= moments["m00"] <= self._dados.area_maxima:
                     c_x = int(moments["m10"] / moments["m00"])
                     c_y = int(moments["m01"] / moments["m00"])
                     centroids = np.vstack(
