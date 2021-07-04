ef26de28fcb45b07d4a1223111e857d9e83b0507
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-07-04 15:43:46 -0300

    add QFileDialog para carregar padrões

--- reddragons/interface/pages/carregar.py
+++ reddragons/interface/pages/carregar.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.uic import loadUi
 
 from ..utils import ui_files
@@ -10,17 +10,10 @@ from ..utils import ui_files
 class GUI_carregar(QMainWindow):
     def __init__(self, visao, model):
         super(GUI_carregar, self).__init__()
-        loadUi(f"{ui_files}/carregar.ui", self)
-        self.show()
-        self.visao = visao
         self.model = model
-        self.QT_listaSalvos.clear()
-        for files in os.listdir("modelos"):
-            if files.endswith(".red"):
-                self.QT_listaSalvos.addItem(str(files))
-        self.QT_carregar.clicked.connect(self.carregar)
+        self.carregar()
 
     def carregar(self):
-
-        arquivo = open("modelos/" + self.QT_listaSalvos.currentText(), "rb")
-        self.model.dados = pickle.load(arquivo)
+        filename = QFileDialog.getOpenFileName(self, "Open File", filter = "*.red")
+        with open(filename[0], "rb") as f:
+            self.model.dados = pickle.load(f)
