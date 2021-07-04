1ef5eef8ea31f5eb6eb4aa59c174f7e1fdd7f69f
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-07-04 15:39:15 -0300

    add QFileDialog para salvar padrões

--- reddragons/interface/pages/salvar.py
+++ reddragons/interface/pages/salvar.py
@@ -1,21 +1,17 @@
 import pickle
 
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.uic import loadUi
-
-from ..utils import ui_files
-
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 
 class GUI_salvar(QMainWindow):
     def __init__(self, visao, model):
         super(GUI_salvar, self).__init__()
-        loadUi(f"{ui_files}/salvar.ui", self)
-        self.show()
         self.model = model
-        self.visao = visao
-        self.QT_salvar.clicked.connect(self.salvar)
+        self.salvar()
 
     def salvar(self):
-        arquivo = open("modelos/" + self.QT_nomeSalvar.text() + ".red", "wb")
+        filename = QFileDialog.getSaveFileName(self, 'Dialog Title', filter='*.red')
+        filename = f"{filename[0]}.{filename[1].split('.')[1]}"
 
-        pickle.dump(self.model.dados, arquivo)
+        with open(filename, "wb") as f:
+            pickle.dump(self.model.dados, f)
+        self.close()
