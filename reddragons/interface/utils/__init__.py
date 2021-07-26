77805d08892585a09ecb4c583cae54d3fbe73026
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-07-26 13:40:24 -0300

    add routing app

--- reddragons/interface/utils/__init__.py
+++ reddragons/interface/utils/__init__.py
@@ -1,3 +1,4 @@
 from pathlib import Path
+from .routing_app import RoutingApp
 
 ui_files = Path(__file__, "../../ui_files").resolve()
---
+++ reddragons/interface/utils/routing_app.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import QMainWindow, QStackedWidget
+
+class RoutingApp (QMainWindow):
+    def __init__ (self):
+        super(RoutingApp, self).__init__()
+        self.stack = QStackedWidget(self)
+        self.setCentralWidget(self.stack)
+        self.hist = []
+        self.pages = {}
+        self.show()
+
+    def register (self, name, widget):
+        index = len(self.pages)
+        self.pages.update({
+            name: (index, widget)
+        })
+        self.stack.addWidget(widget)
+        return index
+
+    def fit(self, widget):
+        fmg = widget.geometry()
+        size = (fmg.width(), fmg.height())
+        print(size)
+        self.resize(*size)
+
+    def push_page (self, page):
+        try:
+            idx, widget = self.pages[page]
+        except:
+            print('falha ao encontrar pagina')
+            raise
+        self.stack.setCurrentIndex(idx)
+        self.fit(widget)
+        self.hist.append(idx)
+    
+    def back (self):
+        if len(self.hist) < 2:
+            return -1
+        self.hist.pop()
+
+        idx = self.hist[-1]
+        widget = dict(self.pages.values())[idx]
+        self.stack.setCurrentIndex(idx)
+        self.fit(widget)
+        return idx
