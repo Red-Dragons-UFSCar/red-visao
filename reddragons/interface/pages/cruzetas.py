71822d93d3a5d74103c77818e3b941defc24711b
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-08-11 21:48:02 -0300

    cruzeas identifica posicoes automaticamente

--- reddragons/interface/pages/cruzetas.py
+++ reddragons/interface/pages/cruzetas.py
@@ -2,9 +2,14 @@ import cv2
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
-
+import numpy as np
 from ..utils import ui_files
 
+def identificaCruzetas (pts):
+     pts_sorted = sorted(pts, key=lambda x: x[1])
+     inferior_esq = pts_sorted[2]
+     superior_esq, superior_dir = sorted(pts_sorted[:2], key=lambda x: x[0])
+     return [superior_esq, superior_dir, inferior_esq]
 
 class GUI_cruzetas(QMainWindow):
     def __init__(self, visao, model):
@@ -14,6 +19,7 @@ class GUI_cruzetas(QMainWindow):
         self.visao = visao
         self.model = model
         self.dados = self.model.dados
+        self.selecoes = list(self.dados.cruzetas)
         self.get_referencia()
         self.QT_btReferencia.clicked.connect(self.get_referencia)
         self.QT_btFinalizar.clicked.connect(self.finalizar)
@@ -24,7 +30,10 @@ class GUI_cruzetas(QMainWindow):
         self.desenhar()
 
     def finalizar(self):
-
+        if len(self.selecoes) < 3:
+            self.selecoes.append([0,0])
+            self.finalizar()
+        self.dados.cruzetas = np.asarray(identificaCruzetas(self.selecoes))
         self.model.dados = self.dados
 
     def mouseReleaseEvent(self, QMouseEvent):
@@ -33,16 +42,21 @@ class GUI_cruzetas(QMainWindow):
         x = _x - self.QT_Imagem.pos().x()
         y = _y - self.QT_Imagem.pos().y()
 
-        if (x < self.QT_Imagem.geometry().width()) and (
-            y < self.QT_Imagem.geometry().height() and x >= 0 and y >= 0
+        if (0 <= x < self.QT_Imagem.geometry().width()) and (
+            0 <= y < self.QT_Imagem.geometry().height()
         ):
-            self.dados.cruzetas[self.QT_posicao.currentIndex()] = [x, y]
+            if len(self.selecoes) == 3:
+                self.selecoes.sort(key = lambda i: (x-i[0])**2 + (y-i[1])**2)
+                self.selecoes[0] = [x,y]
+            else:
+                self.dados.cruzetas[self.QT_posicao.currentIndex()] = [x, y]
+                self.selecoes.append([x,y])
             self.desenhar()
 
     def desenhar(self):
         img = self.referencia.copy()
 
-        for ponto in self.dados.cruzetas:
+        for ponto in self.selecoes:
             cv2.drawMarker(img, (ponto[0], ponto[1]), (255, 0, 0))
 
         _q_image = QImage(img, img.shape[1], img.shape[0], QImage.Format_RGB888)
