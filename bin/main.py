7e777db4094dc827d35f7d52a2b47c70ed40b8ea
Vitor Diniz <vitordiniz2010@hotmail.com>
2021-11-15 15:07:03 -0300

    atualizado main.py para versão nova

--- bin/main.py
+++ bin/main.py
@@ -1,17 +1,22 @@
 from PyQt5.QtWidgets import QApplication
+import sys
+from reddragons.interface import GUI_video, VisaoTop
+from reddragons.utils import Logger
 from reddragons.visao import Processamento
 from reddragons.estruturas import ModelService
-import sys
-from reddragons.interface import GUI_main
-from reddragons.utils import Logger
-from reddragons.utils import VIDEO_PATH
 
-if __name__ == '__main__':
+def init_app ():
     model = ModelService()
     visao = Processamento(model)
-    visao.alterar_src(VIDEO_PATH)
+
+    app = VisaoTop(visao, model)
+    app.push_widget(GUI_video(app))
+    return app
+
+if __name__ == '__main__':
+    
     app = QApplication(sys.argv)
-    window = GUI_main(visao, model)
+    window = init_app()
     window.show()
     print (Logger()._fps_mean)
     sys.exit(app.exec_())
